e2922d27e87698fa30b48934b01892ef2be82436
xxh <2103309470@qq.com>
2022-07-09 14:21:54 +0800

    feat: 异步处理以及异步取消Demo

---
+++ AsyncAwaitDemo/AsyncAwaitDemo/AsyncDemo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitDemo
+{
+    public class AsyncDemo
+    {
+
+        public static async Task FileAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"await 之前，线程Id为{Thread.CurrentThread.ManagedThreadId.ToString()}");
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 10000000; i++)
+            {
+                sb.AppendLine("hello");
+            }
+            var filename = "d:\\a\\1.txt";
+            await File.WriteAllTextAsync(filename, sb.ToString());
+            //await File.WriteAllTextAsync(filename, sb.ToString(), cancellationToken);
+            Console.WriteLine($"await 之后111，线程Id为{Thread.CurrentThread.ManagedThreadId.ToString()}");
+
+            //if(cancellationToken.IsCancellationRequested)
+            //{
+            //    Console.WriteLine($"线程已取消");
+            //}
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+           await File.ReadAllTextAsync(filename);
+            Console.WriteLine($"await 之后222，线程Id为{Thread.CurrentThread.ManagedThreadId.ToString()}");
+        }
+    }
+}
---
+++ AsyncAwaitDemo/AsyncAwaitDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//杨中科老师举例餐馆点菜的例子来说明异步的这个事还是很形象的
+//不能保证await 前后的线程是否是同一个。await只是等待了，然后线程会回到线程池。
+//尽量不要使用Thread.Sleep  而是使用 await Task.Delay();
+using AsyncAwaitDemo;
+
+
+Console.WriteLine($"program，线程Id为{Thread.CurrentThread.ManagedThreadId.ToString()}");
+
+//线程取消
+CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+cancellationTokenSource.CancelAfter(1000);
+await AsyncDemo.FileAsync(cancellationTokenSource.Token);
+Console.WriteLine($"program111，线程Id为{Thread.CurrentThread.ManagedThreadId.ToString()}");
+
+Console.ReadKey();
